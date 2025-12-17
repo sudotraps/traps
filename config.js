6f29334d9501191c3dd50f555c27542f89f318f8
T R A P S <javieralonsosaucedarubio@gmail.com>
2025-12-16 17:15:27 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// Only declare if it doesn't exist yet
+// config.js
 const SUPABASE_URL = 'https://txhofuhrcvmskbrbvcit.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_fU3lmsGzsLld4xzqzfZG8w_ENZMM6_y';
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-if (typeof supabase === 'undefined') {
-    var supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-}
