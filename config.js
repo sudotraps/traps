1a96178a3d303edf1dab1a8603696416166f94cc
T R A P S <javieralonsosaucedarubio@gmail.com>
2025-12-16 17:15:44 -0700

    Remove commented-out config lines

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// config.js
 const SUPABASE_URL = 'https://txhofuhrcvmskbrbvcit.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_fU3lmsGzsLld4xzqzfZG8w_ENZMM6_y';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
 
