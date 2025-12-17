d75f51b7cb9e8959023044c0a782dd27e8ac0e3b
T R A P S <javieralonsosaucedarubio@gmail.com>
2025-12-16 17:02:30 -0700

    Reformat config.js with consistent syntax

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// ⚠️ REEMPLAZA ESTOS VALORES CON LOS TUYOS DE SUPABASE
 const SUPABASE_URL = 'https://txhofuhrcvmskbrbvcit.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_fU3lmsGzsLld4xzqzfZG8w_ENZMM6_y';
 
@@ -7,3 +6,4 @@ const ADMIN_PASSWORD = 'Sudo.Paputraps';
 
 // Crear cliente de Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
